b73117471711cc1ed4a7cff77902367cf1f73788
frankron <u20181b468@upc.edu.pe>
2024-05-10 15:20:38 -0500

    franki

--- 01.js
+++ 01.js
@@ -1,1 +1,28 @@
-frank
+function descargarnuevosclientes(){
+
+    return new Promise( resolve =>{
+
+        console.log('Desacrgando clientes ... espere...')
+
+        setTimeout(()=>{
+
+            resolve('Los clientes fueron Descargados');
+        },5000 );
+
+    });
+
+}
+
+async function app(){
+    
+  try{
+      const resultado= descargarnuevosclientes();
+console.log(resultado);
+  }
+  catch(error)
+  {
+    console.log(error);
+  }
+
+}
+app();
